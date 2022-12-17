b0d16094df5380671451cc049037c8d8eda3fb62
My_Pure <cyn761480091@qq.com>
2022-12-17 22:36:26 +0800

    change version

--- src/Monitor/PluginInfo.cs
+++ src/Monitor/PluginInfo.cs
@@ -4,5 +4,5 @@ public static class PluginInfo
 {
     public const string PLUGIN_GUID = "com.mypure.cardessential.monitor";
     public const string PLUGIN_NAME = "CardEssential.Monitor";
-    public const string PLUGIN_VERSION = "0.0.1";
+    public const string PLUGIN_VERSION = "0.0.2";
 }
